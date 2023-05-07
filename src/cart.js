54248748bc2caf8030eba140a77633f4cbcd8ea9
fedetempone <fedetempone@gmail.com>
2023-05-07 05:00:05 -0300

    correcciones generales

---
+++ src/cart.js
@@ -0,0 +1,8 @@
+// const productCartPrice = document.querySelector('.productCartPrice'); //el precio del producto que se va a mostrar
+// const inputNumberCart = document.querySelector('.inputNumberCart'); // el input de la cantidad de productos que va a querer el usuario
+// const menuCart = document.querySelector('.funkoColeccion'); // este es el div entero del producto que se va a mostrar al seleccionar
+
+
+// menuCart.addEventListener('click', (e) => {
+
+// })
---
+++ src/shop.js
@@ -0,0 +1,18 @@
+// const trooper = document.querySelector('.imgTrooper');
+// const pokemon = document.querySelector('.imgPokemon');
+// const lunaHP = document.querySelector('.imgHP');
+// const menuCart = document.querySelector('.funkoColeccion'); // este es el div entero del producto que se va a mostrar al seleccionar
+
+// trooper.addEventListener('click', addToCart);
+
+// function addToCart (){
+//     menuCart.classList.add('active');
+// }
+
+// console.log(menuCart);
+// console.log(trooper);
+// console.log(pokemon);
+// console.log(lunaHP);
+
+// var getImport = document.querySelector('#template-file');
+// var getContent = getImport.import.querySelector('#content'); 
